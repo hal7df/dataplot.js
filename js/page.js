02fd5793bff584035a0dc1976cf70f6e3ba0f15c
Paul Bunyan <hal7df@gmail.com>
2017-03-28 22:42:56 -0400

    Make the autoscale checkboxes enable/disable manual ranges.
    Updates to interfacing with the save data.

--- js/page.js
+++ js/page.js
@@ -2,13 +2,14 @@ var page = {
 	init: function () {
 		this.readSaveData(document.getElementById("config"));
 		this._graph = new Graph (document.getElementById("graph"));
+		this.redraw();
 	},
 	redraw: function () {
 		this._graph.setTitle(this._data.plot.title);
 		this._graph.setXLabel(this._data.plot.xAxis.label);
 		this._graph.setYLabel(this._data.plot.yAxis.label);
 		this._graph.drawGraph();
-	}
+	},
 	readSaveData: function (inData) {
 		if (inData.nodeType) {			
 			this._data = {
@@ -16,15 +17,15 @@ var page = {
 					title: inData.elements.namedItem("graph-title").value,
 					xAxis: {
 						label: document.getElementById("x-name").textContent,
-						autoscale: inData.elements.namedItem("autoscale-x").checked,
-						min: inData.elements.namedItem("x-min").value,
-						max: inData.elements.namedItem("x-max").value
+						autoscale: inData.elements.namedItem("x-autoscale").checked,
+						min: parseInt(inData.elements.namedItem("x-min").value, 10),
+						max: parseInt(inData.elements.namedItem("x-max").value, 10)
 					},
 					yAxis: {
 						label: inData.elements.namedItem("graph-ylabel").value,
-						autoscale: inData.elements.namedItem("autoscale-y").checked,
-						min: inData.elements.namedItem("y-min").value,
-						max: inData.elements.namedItem("y-max").value
+						autoscale: inData.elements.namedItem("y-autoscale").checked,
+						min: parseInt(inData.elements.namedItem("y-min").value, 10),
+						max: parseInt(inData.elements.namedItem("y-max").value, 10)
 					}
 				},
 				data: {
@@ -39,14 +40,24 @@ var page = {
 
 			var config = document.getElementById("config").elements;
 			config.namedItem("graph-title").value = this._data.plot.title;
+			config.namedItem("graph-title").parentElement.classList.add("is-dirty");
 			document.getElementById("x-name").textContent = this._data.plot.xAxis.label;
-			config.namedItem("autoscale-x").checked = this._data.plot.xAxis.autoscale;
-			config.namedItem("x-min").value = this._data.plot.xAxis.min;
-			config.namedItem("x-max").value = this._data.plot.xAxis.max;
+			config.namedItem("x-autoscale").checked = this._data.plot.xAxis.autoscale;
+			if (!this._data.plot.xAxis.autoscale) {
+				config.namedItem("x-min").value = this._data.plot.xAxis.min;
+				config.namedItem("x-min").parentElement.classList.add("is-dirty");
+				config.namedItem("x-max").value = this._data.plot.xAxis.max;
+				config.namedItem("x-max").parentElement.classList.add("is-dirty");
+			}
 			config.namedItem("graph-ylabel").value = this._data.plot.yAxis.label;
-			config.namedItem("autoscale-y").value = this._data.plot.yAxis.autoscale;
-			config.namedItem("y-min").value = this._data.plot.yAxis.min;
-			config.namedItem("y-max").value = this._data.plot.yAxis.max;
+			config.namedItem("graph-ylabel").parentElement.classList.add("is-dirty");
+			config.namedItem("y-autoscale").value = this._data.plot.yAxis.autoscale;
+			if (!this._data.plot.yAxis.autoscale) {
+				config.namedItem("y-min").value = this._data.plot.yAxis.min;
+				config.namedItem("y-min").parentElement.classList.add("is-dirty");
+				config.namedItem("y-max").value = this._data.plot.yAxis.max;
+				config.namedItem("y-min").parentElement.classList.add("is-dirty");
+			}
 		}
 	},
 	settings: {
@@ -59,6 +70,20 @@ var page = {
 			event = event || window.event;
 			page._graph.setYLabel(page._data.plot.yAxis.label = event.target.value);
 			page._graph.drawGraph();
+		},
+		autoscale: function (event) {
+			event = event || window.event
+			var min = event.target.parentElement.nextElementSibling.firstElementChild;
+			var max = min.parentElement.nextElementSibling.firstElementChild;
+			var axis = page._data.plot[event.target.id.split('-')[0] === 'x' ? "xAxis" : "yAxis"];
+			
+			min.disabled = event.target.checked;
+			max.disabled = event.target.checked;
+			
+			if (event.target.checked) {
+				axis.min = parseInt(min.value = "", 10);
+				axis.max = parseInt(max.value = "", 10);
+			}
 		}
 	},
 	datasets: {
