c98fcf50176308176610e69858c5c08eaba6a89c
Paul Bunyan <hal7df@gmail.com>
2017-03-31 22:44:49 -0400

    Add rows to the datasets when the last row is filled up.
    - Mask user input to the rows.
    - Add button for user to set a constant uncertainty for a whole column.
    
    TODO:
    - Make the table scroll properly/make columns fit properly.
    - Probably ought to revisit the row adding code, or redesign it so it
    works a little more intuitively

--- js/page.js
+++ js/page.js
@@ -18,9 +18,16 @@
 
 var page = {
 	init: function () {
+		//Draw initial graph
 		this.readSaveData(document.getElementById("config"));
 		this._graph = new Graph (document.getElementById("graph"));
 		this.redraw();
+		
+		//Populate list of point types
+		var numPoints = 7; //should match the number of pt-*.svg files in ../img/
+		for (var i = 0; i < numPoints; ++i) {
+			ajaxGet("https://raw.githubusercontent.com/hal7df/dataplot.js/beta/img/pt-" + i + ".svg", this._savePointType(i));
+		}
 	},
 	redraw: function () {
 		this._graph.setTitle(this._data.plot.title);
@@ -109,8 +116,106 @@ var page = {
 			event = event || window.event;
 			page._graph.setXLabel(page._data.plot.xAxis.label = event.target.textContent);
 			page._graph.drawGraph();
+		},
+		mask: function (event) {
+			event = event || window.event;
+			var key = event.keycode || event.which;
+			var validKey = (key >= 48 && key <= 57); //number row
+			validKey |= (key >= 96 && key <= 105); //numpad
+			validKey |= key === 189 || key === 109 || key == 173; //minus sign
+			validKey |= key === 190 || key === 110; //decimal point
+			validKey |= key >= 35 && key <= 40; //navigation keys
+			validKey |= key === 9 || key === 13; //tab and enter
+			validKey |= key === 8 || key === 46; //backspace and delete
+			validKey |= key === 85; //letter 'u' (for uncertainty)
+			validKey |= ((key === 86 || key === 67 || key == 88) &&
+						(event.ctrlKey || event.metaKey)); //clipboard actions
+			
+			if (!validKey) event.preventDefault();
+			if (key == 85) {
+				if (event.target.textContent.indexOf('\u00B1') === -1) {
+					event.target.textContent += '\u00B1';
+					var range = document.createRange();
+					var sel = window.getSelection();
+					range.setStart(event.target.childNodes[0], event.target.textContent.length);
+					range.collapse(true);
+					sel.removeAllRanges();
+					sel.addRange(range);
+				}
+				event.preventDefault();
+			}
+		},
+		update: function (event) {
+			event = event || window.event;
+			var row = parseInt(event.target.parentElement.dataset.rownum, 10);
+			var data = page._data.data;
+			var dataNeeded = event.target.parentElement.dataset.needed;
+			var yTest = function (arr) {
+				return !(arr[row] === null || arr[row] === undefined);
+			};
+			
+			if (event.target.classList.contains('x')) {
+				if (event.target.textContent.length > 0) {
+					data.x[row] = parseFloat(event.target.textContent, 10);
+					if (dataNeeded) dataNeeded = dataNeeded.replace("x","");
+				} else if (data.x[row] !== null || data.x[row] !== undefined)
+					data.x[row] = null;
+			} else {
+				var setnum = event.target.dataset.setnum;
+				if (data.y[setnum] === undefined) data.y[setnum] = [];
+				if (event.target.textContent.length > 0) {
+					data.y[setnum][row] = parseFloat(event.target.textContent, 10);
+					if (dataNeeded) dataNeeded = dataNeeded.replace("y","");
+				} else if (data.y[setnum][row] !== null || data.x[row] !== undefined)
+					data.y[setnum][row] = null;
+			}
+			
+			if (dataNeeded !== undefined && dataNeeded.length === 0) {
+				dataNeeded = undefined;
+				page.datasets.newRow(event.target.parentElement.parentElement);
+			}
+			
+			event.target.parentElement.dataset.needed = dataNeeded;
+		},
+		newRow: function (table) {
+			var row = document.createElement("tr");
+			row.classList.add("new-row");
+			row.dataset.rownum = parseInt(table.lastElementChild.dataset.rownum, 10) + 1;
+			row.dataset.needed = "xy";
+			
+			var createCell = function (colnum) {
+				var cell = document.createElement("td");
+				cell.contentEditable = "true";
+				cell.onblur = page.datasets.update;
+				cell.onkeydown = page.datasets.mask;
+				cell.classList.add(colnum === undefined ? "x" : "y");
+				if (colnum !== undefined) cell.dataset.setnum = colnum;
+				return cell;
+			};
+			
+			row.appendChild(createCell());
+			
+			for (var i = 0; i < page._data.data.y.length; ++i)
+				row.appendChild(createCell(i));
+			
+			table.appendChild(row);
 		}
+	},
+	_points: [],
+	_savePointType: function (index) {
+		return function (svg) { page._points[index] = svg; }
 	}
 };
+
+function ajaxGet (url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url);
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status < 400) {
+			callback(xhr.responseText);
+		}
+	};
+	xhr.send();
+}
 
 window.onload = page.init.bind(page);
