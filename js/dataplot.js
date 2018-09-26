df306c222a5c7d2c493dc1ab0a2ed0877cf4e5fd
Paul Bunyan <hal7df@gmail.com>
2018-09-26 10:32:31 -0400

    Refactor the Graph class.
    - Use closures to actually enforce a private access level.
    - Ignore vim *.swp files.

--- js/dataplot.js
+++ js/dataplot.js
@@ -21,120 +21,197 @@
  * limitations under the License.
  */
 
-function Graph (canvas) {	
-	this.ctx = canvas.getContext("2d");
-	this.plotArea = {x:{min:0,max:0},y:{min:0,max:0}};
-	this.plotArea.width = function () {return this.x.max - this.x.min;};
-	this.plotArea.height = function () {return this.y.max - this.y.min;};
-	this._window = {
+function Graph (canvas) {
+        /* Private members */
+	var m_ctx = canvas.getContext("2d");
+	var m_plotArea = {x:{min:0,max:0},y:{min:0,max:0}};
+	var m_lables = {x:"", y:"", title:""};
+	var m_labelSizes = {};
+	var m_window = {
 			x:{min:0, max:10, interval: 1},
 			y:{min:0, max:10, interval: 1}
 	};
-	this._window.width = function () { return this.x.max - this.x.min; };
-	this._window.height = function () { return this.y.max - this.y.min; };
-	this._labels = {x:"", y:"", title:""};
-	this._labelSizes = {};
 	
-	this.setTitle = function (title)  { this._labels.title = title; };
-	this.setXLabel = function (xlabel) { this._labels.x = xlabel; };
-	this.setYLabel = function (ylabel) { this._labels.y = ylabel; };
+        m_plotArea.width = function () {return this.x.max - this.x.min;};
+	m_plotArea.height = function () {return this.y.max - this.y.min;};
+	m_window.width = function () { return this.x.max - this.x.min; };
+	m_window.height = function () { return this.y.max - this.y.min; };
+	
+        var calcInterval = function (windowWidth, plotWidth, minGridSpacing) {
+            var smallestInterval, preferredInterval = undefined;
+            var ticks = 0;
+            
+            while ((plotWidth/(++ticks)) >= minGridSpacing) {
+                    smallestInterval = windowWidth/ticks;
+                    
+                    if (smallestInterval % 1 == 0)
+                            preferredInterval = smallestInterval;
+            }
+            
+            return preferredInterval || smallestInterval;
+	};
+
+	var calcWindow = function () {
+            //Calculate unit spacing
+            m_window.x.spacing = (m_plotArea.width()/m_window.width());
+            m_window.y.spacing = (m_plotArea.height()/m_window.height());
+            
+            //Get the graph tick interval
+            var minSpacing = 0.05;
+            minSpacing *= m_plotArea.width() > m_plotArea.height() ? 
+                            m_plotArea.width() : m_plotArea.height(); 
+            m_window.x.interval = calcInterval(m_window.width(),
+                                               m_plotArea.width(), minSpacing);
+            m_window.y.interval = calcInterval(m_window.height(),
+                                               m_plotArea.height(), minSpacing);
+	};
+
+        var reset = function () {
+		m_ctx.strokeStyle = "#000000";
+		m_ctx.fillStyle = "#000000";
+		m_ctx.font = "10px sans-serif";
+		m_ctx.textBaseline = "middle";
+		m_ctx.textAlign = "center";
+		m_ctx.lineWidth = "1";
+		m_ctx.setLineDash([]);
+		
+		calcWindow();
+	};
+
+        var getVisibleRange = function (min, max) {
+            var d = max - min;
+            var h = 10;
+
+            if ((d - 0) > (Number.EPSILON*8))
+                h = ((d/0.9) - d)/2;
+
+            return {
+                min: (min - h),
+                max: (max + h)
+            };
+        };
+
+	var x_ = function (x) {
+            if (x >= m_window.x.min && x <= m_window.x.max) {
+                    return m_plotArea.x.min + ((x - m_window.x.min) * (m_plotArea.width()/m_window.width()));
+	    }
+            else return -1;
+	};
+
+	var y_ = function (y) {
+		if (y >= m_window.y.min && y <= m_window.y.max) {
+			return m_plotArea.y.max - ((y - m_window.y.min) * (m_plotArea.height()/m_window.height()));
+		}
+		else return -1;
+	};
+	
+	/* Public members */
+        this.setTitle = function (title)  { m_lables.title = title; };
+	this.setXLabel = function (xlabel) { m_lables.x = xlabel; };
+	this.setYLabel = function (ylabel) { m_lables.y = ylabel; };
 	this.setXWindow = function (min,max) {
-		this._window.x.min = min;
-		this._window.x.max = max;
+		m_window.x.min = min;
+		m_window.x.max = max;
 	};
 	this.setYWindow = function (min,max) {
-		this._window.y.min = min;
-		this._window.y.max = max;
+		m_window.y.min = min;
+		m_window.y.max = max;
 	};
+        this.autoscaleX = function (data) {
+        };
+        this.autoscaleY = function (data) {
+        };
 	this.clear = function () {
-		this.ctx.fillStyle = "#ffffff";
-		this.ctx.fillRect(0,0,canvas.width,canvas.height);
-		this._reset();
+		m_ctx.fillStyle = "#ffffff";
+		m_ctx.fillRect(0,0,canvas.width,canvas.height);
+		reset();
 	};
 	this.drawGraph = function () {
 		this.clear();
 		
 		//Draw outer bounds
-		this.ctx.lineWidth = "2";
-		this.ctx.strokeRect(this.plotArea.x.min,this.plotArea.y.min,
-							this.plotArea.width(), this.plotArea.height());
+		m_ctx.lineWidth = "2";
+		m_ctx.strokeRect(m_plotArea.x.min, m_plotArea.y.min, m_plotArea.width(),
+                               m_plotArea.height());
 		
 		//Calculate label positions
 		var plotCenter = {};
-		plotCenter.x = this.plotArea.x.min + (this.plotArea.width()/2);
-		plotCenter.y = this.plotArea.y.min + (this.plotArea.height()/2);
+		plotCenter.x = m_plotArea.x.min + (m_plotArea.width()/2);
+		plotCenter.y = m_plotArea.y.min + (m_plotArea.height()/2);
 		
-		//Draw title and axis labels
-		this.ctx.font = this._labelSizes.title.toString() + "px sans-serif";
-		this.ctx.fillText(this._labels.title, plotCenter.x, (this.plotArea.y.min/2));
-		this.ctx.font = this._labelSizes.axis.toString() + "px sans-serif";
-		this.ctx.fillText(this._labels.x, plotCenter.x,
-							(this.plotArea.y.max + ((canvas.height - this.plotArea.y.max)/1.5)));
-		this.ctx.save();
-		this.ctx.rotate(-Math.PI/2);
-		this.ctx.fillText(this._labels.y, -plotCenter.y, this.plotArea.y.min/2);
-		this.ctx.restore();
+		//Draw title and axis m_lables
+		m_ctx.font = m_labelSizes.title.toString() + "px sans-serif";
+		m_ctx.fillText(m_lables.title, plotCenter.x, (m_plotArea.y.min/2));
+		m_ctx.font = m_labelSizes.axis.toString() + "px sans-serif";
+		m_ctx.fillText(m_lables.x, plotCenter.x,
+                             (m_plotArea.y.max + ((canvas.height - m_plotArea.y.max)/1.5)));
+
+		m_ctx.save();
+		m_ctx.rotate(-Math.PI/2);
+		m_ctx.fillText(m_lables.y, -plotCenter.y, m_plotArea.y.min/2);
+		m_ctx.restore();
 		
 		//Initialize context for grid lines
-		this.ctx.strokeStyle = "#878787";
-		this.ctx.lineWidth = "1";
-		this.ctx.setLineDash([3,3]);
-		this.ctx.font = (this._labelSizes.axis/1.5).toString() + "px sans-serif";
+		m_ctx.strokeStyle = "#878787";
+		m_ctx.lineWidth = "1";
+		m_ctx.setLineDash([3,3]);
+		m_ctx.font = (m_labelSizes.axis/1.5).toString() + "px sans-serif";
 		
 		//Draw x-axis grid lines
-		this.ctx.textBaseline = "top";
-		for (var x = this._window.x.min; x <= this._window.x.max; x += this._window.x.interval) {
-			if (x > this._window.x.min && x < this._window.x.max) {
-				this.ctx.beginPath();
-				this.ctx.moveTo(this._x(x),this._y(this._window.y.min));
-				this.ctx.lineTo(this._x(x),this._y(this._window.y.max));
-				this.ctx.stroke();
-			}
-			
-			this.ctx.fillText(x % 1 === 0 ? x : x.toFixed(1), this._x(x),
-								this._y(this._window.y.min) + 5);
+		m_ctx.textBaseline = "top";
+		for (var x = m_window.x.min; x <= m_window.x.max;
+                     x += m_window.x.interval) {
+                    if (x > m_window.x.min && x < m_window.x.max) {
+                        m_ctx.beginPath();
+                        m_ctx.moveTo(x_(x), y_(m_window.y.min));
+                        m_ctx.lineTo(x_(x), y_(m_window.y.max));
+                        m_ctx.stroke();
+                    }
+                    
+                    m_ctx.fillText(((x % 1 === 0) ? x : x.toFixed(1)), x_(x),
+                                 (y_(m_window.y.min) + 5));
 		}
 		
 		//Draw y-axis grid lines
-		this.ctx.textBaseline = "middle";
-		this.ctx.textAlign = "right";
-		for (var y = this._window.y.min; y <= this._window.y.max; y += this._window.y.interval) {
-			if (y > this._window.y.min && y < this._window.y.max) {
-				this.ctx.beginPath();
-				this.ctx.moveTo(this._x(this._window.x.min),this._y(y));
-				this.ctx.lineTo(this._x(this._window.x.max),this._y(y));
-				this.ctx.stroke();
-			}
-			
-			this.ctx.fillText(y % 1 === 0 ? y : y.toFixed(1),
-								this._x(this._window.x.min) - 5, this._y(y));
+		m_ctx.textBaseline = "middle";
+		m_ctx.textAlign = "right";
+		for (var y = m_window.y.min; y <= m_window.y.max; y += m_window.y.interval) {
+                    if (y > m_window.y.min && y < m_window.y.max) {
+                        m_ctx.beginPath();
+                        m_ctx.moveTo(x_(m_window.x.min), y_(y));
+                        m_ctx.lineTo(x_(m_window.x.max), y_(y));
+                        m_ctx.stroke();
+                    }
+                    
+                    m_ctx.fillText(((y % 1 === 0) ? y : y.toFixed(1)),
+                                 (x_(m_window.x.min) - 5), y_(y));
 		}
 		
 		//Reset context
-		this._reset();
+		reset();
 	};
 	this.plotImage = function (img, x, y) {
-		if (this._x(x) < 0 || this._y(y) < 0) return;
+		if (x_(x) < 0 || y_(y) < 0) return;
 		
-		this.ctx.drawImage(img, this._x(x) - (img.width/2),
-							this._y(y) - (img.height/2));
+		m_ctx.drawImage(img, x_(x) - (img.width/2),
+                                y_(y) - (img.height/2));
 	};
 	this.startLine = function (x, y, color) {
-		if (this._x(x) < 0 || this._y(y) < 0) return;
+		if (x_(x) < 0 || y_(y) < 0) return;
 		
-		this.ctx.strokeStyle = color === undefined ? "#000000" : color;
-		this.ctx.lineWidth = "1";
-		this.ctx.setLineDash([]);
-		this.ctx.beginPath();
-		this.ctx.moveTo(this._x(x), this._y(y));
+		m_ctx.strokeStyle = (color === undefined ? "#000000" : color);
+		m_ctx.lineWidth = "1";
+		m_ctx.setLineDash([]);
+		m_ctx.beginPath();
+		m_ctx.moveTo(x_(x), y_(y));
 	};
 	this.addLinePoint = function (x, y) {
-		if (this._x(x) < 0 || this._y(y) < 0) return;
+		if (x_(x) < 0 || y_(y) < 0) return;
 		
-		this.ctx.lineTo(this._x(x), this._y(y));
+		m_ctx.lineTo(x_(x), y_(y));
 	};
 	this.drawLine = function () {
-		this.ctx.stroke();
+		m_ctx.stroke();
 	};
 	this.updateSize = function () {
 		//Update canvas size
@@ -142,68 +219,16 @@ function Graph (canvas) {
 		canvas.height = parseInt(window.getComputedStyle(canvas).height);
 		
 		//Set plot area
-		this.plotArea.x.min = (0.075*canvas.width);
-		this.plotArea.x.max = (0.975*canvas.width);
-		this.plotArea.y.min = (0.075*canvas.height);
-		this.plotArea.y.max = (0.925*canvas.height);
+		m_plotArea.x.min = (0.075*canvas.width);
+		m_plotArea.x.max = (0.975*canvas.width);
+		m_plotArea.y.min = (0.075*canvas.height);
+		m_plotArea.y.max = (0.925*canvas.height);
 
 		//Set label sizes
-		this._labelSizes.title = (this.plotArea.y.min)/2;
-		this._labelSizes.axis = (canvas.height - this.plotArea.y.max)/3;
+		m_labelSizes.title = (m_plotArea.y.min)/2;
+		m_labelSizes.axis = (canvas.height - m_plotArea.y.max)/3;
 	};
-	this._reset = function () {
-		this.ctx.strokeStyle = "#000000";
-		this.ctx.fillStyle = "#000000";
-		this.ctx.font = "10px sans-serif";
-		this.ctx.textBaseline = "middle";
-		this.ctx.textAlign = "center";
-		this.ctx.lineWidth = "1";
-		this.ctx.setLineDash([]);
-		
-		this._calcWindow();
-	};
-	this._calcWindow = function () {
-		//Calculate unit spacing
-		this._window.x.spacing = (this.plotArea.width()/this._window.width());
-		this._window.y.spacing = (this.plotArea.height()/this._window.height());
-		
-		//Get the graph tick interval
-		var minSpacing = 0.05;
-		minSpacing *= this.plotArea.width() > this.plotArea.height() ? 
-				this.plotArea.width() : this.plotArea.height(); 
-		this._window.x.interval = this._calcInterval(this._window.width(),
-														this.plotArea.width(),
-														minSpacing);
-		this._window.y.interval = this._calcInterval(this._window.height(),
-														this.plotArea.height(),
-														minSpacing);
-	};
-	this._calcInterval = function (windowWidth, plotWidth, minGridSpacing) {
-		var smallestInterval, preferredInterval = undefined;
-		var ticks = 0;
-		
-		while ((plotWidth/(++ticks)) >= minGridSpacing) {
-			smallestInterval = windowWidth/ticks;
-			
-			if (smallestInterval % 1 == 0)
-				preferredInterval = smallestInterval;
-		}
-		
-		return preferredInterval || smallestInterval;
-	};
-	this._x = function (x) {
-		if (x >= this._window.x.min && x <= this._window.x.max) {
-			return this.plotArea.x.min + ((x - this._window.x.min) * (this.plotArea.width()/this._window.width()));
-	    }
-		else return -1;
-	};
-	this._y = function (y) {
-		if (y >= this._window.y.min && y <= this._window.y.max) {
-			return this.plotArea.y.max - ((y - this._window.y.min) * (this.plotArea.height()/this._window.height()));
-		}
-		else return -1;
-	}
 	
 	this.updateSize();
-	this._calcWindow();
+	calcWindow();
 }
